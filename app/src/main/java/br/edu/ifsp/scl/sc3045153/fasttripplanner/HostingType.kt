e28039ef538a31b44386ae42727ac361cc07156d
RaulggSouza <raulggsouza@hotmail.com>
2026-04-27 23:26:05 -0300

    feat: create enum for HostingType

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3045153/fasttripplanner/HostingType.kt
@@ -0,0 +1,7 @@
+package br.edu.ifsp.scl.sc3045153.fasttripplanner
+
+enum class HostingType {
+    ECONOMIC,
+    COMFORT,
+    LUXURY
+}
--- app/src/main/java/br/edu/ifsp/scl/sc3045153/fasttripplanner/TripOptionsActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3045153/fasttripplanner/TripOptionsActivity.kt
@@ -6,7 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import br.edu.ifsp.scl.sc3045153.fasttripplanner.ui.theme.FastTripPlannerTheme
 
 class TripOptionsActivity : ComponentActivity() {
@@ -19,5 +25,18 @@ class TripOptionsActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun TripOptionsScreen(modifier: Modifier = Modifier) {
+    var selectedOption by rememberSaveable { mutableStateOf<HostingType?>(null) }
+}
+
+@Preview
+@Composable
+private fun TripOptionsScreenPrev() {
+    FastTripPlannerTheme {
+        TripOptionsScreen()
     }
 }
