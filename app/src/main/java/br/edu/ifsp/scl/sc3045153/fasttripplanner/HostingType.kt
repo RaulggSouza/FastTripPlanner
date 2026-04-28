208afa6c53e008991da147f6f651adf81933020d
RaulggSouza <raulggsouza@hotmail.com>
2026-04-27 23:57:23 -0300

    refactor: makes visualization better

--- app/src/main/java/br/edu/ifsp/scl/sc3045153/fasttripplanner/HostingType.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3045153/fasttripplanner/HostingType.kt
@@ -1,6 +1,6 @@
 package br.edu.ifsp.scl.sc3045153.fasttripplanner
 
-    enum class HostingType(val label: String) {
+enum class HostingType(val label: String) {
     ECONOMIC("Econômica"),
     COMFORT("Conforto"),
     LUXURY("Luxo")
--- app/src/main/java/br/edu/ifsp/scl/sc3045153/fasttripplanner/TripOptionsActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3045153/fasttripplanner/TripOptionsActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
@@ -21,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,6 +38,7 @@ class TripOptionsActivity : ComponentActivity() {
         setContent {
             FastTripPlannerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TripOptionsScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -41,15 +47,31 @@ class TripOptionsActivity : ComponentActivity() {
 
 @Composable
 fun TripOptionsScreen(modifier: Modifier = Modifier) {
+    //Tipo de acomadações marcado para não mudar quando a tela girar
     var selectedHostingType by rememberSaveable { mutableStateOf<HostingType?>(null) }
 
-    Column(modifier = modifier.fillMaxSize().padding(16.dp)) {
-        Text("Escolha o tipo de hospedagem", fontWeight = FontWeight.Bold, fontSize = 30.sp)
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        //Cria spacer para melhor visualização
+        Spacer(modifier = modifier.height(20.dp))
 
+        Text(
+            "Escolha o tipo de hospedagem",
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+        )
+
+        //Itera por cada item do enum
         HostingType.entries.forEach { hostingType ->
             Row(
-                verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = modifier.width(200.dp)
             ) {
+                //Cria um radio button para cada elemento do enum
                 RadioButton(
                     selected = selectedHostingType == hostingType,
                     onClick = {
@@ -60,10 +82,12 @@ fun TripOptionsScreen(modifier: Modifier = Modifier) {
                 Text(hostingType.label, fontSize = 20.sp)
             }
         }
+
+        Spacer(modifier = modifier.height(20.dp))
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun TripOptionsScreenPrev() {
     FastTripPlannerTheme {
