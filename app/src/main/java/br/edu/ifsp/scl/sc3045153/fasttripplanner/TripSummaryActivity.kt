b60bd39a06594bea65e78476de852e53f6740792
RaulggSouza <raulggsouza@hotmail.com>
2026-05-01 21:52:38 -0300

    feat: create screen and preview for TripSummary

--- app/src/main/java/br/edu/ifsp/scl/sc3045153/fasttripplanner/TripSummaryActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3045153/fasttripplanner/TripSummaryActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import br.edu.ifsp.scl.sc3045153.fasttripplanner.ui.theme.FastTripPlannerTheme
 
 class TripSummaryActivity : ComponentActivity() {
@@ -16,9 +19,20 @@ class TripSummaryActivity : ComponentActivity() {
         setContent {
             FastTripPlannerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    TripSummaryScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
+}
+
+@Composable
+fun TripSummaryScreen(modifier: Modifier = Modifier) {
+    
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TripSummaryPreview() {
+    TripSummaryScreen()
 }
